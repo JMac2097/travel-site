4848a2c287241f21b89cfe4c55183143f6bd8da8
John <therealjmac2097@gmail.com>
2017-11-21 21:13:22 +0000

    Some setup for gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+
+gulp.task('default', function(){
+    console.log('Hooray, you created a gulp task!!');
+});
+
+gulp.task('html', function() {
+    console.log('Imagine this doing something very useful indeed');
+});
+
+gulp.task('styles', function() {
+    console.log('Imagine this doing something very useful with that stylesheet');
+});
+
+gulp.task('watch', function() {
+
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles');
+    });
+
+});
