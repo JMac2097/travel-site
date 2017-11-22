9fc0e6e48381400087431492421f77421a6bd402
John <therealjmac2097@gmail.com>
2017-11-22 21:23:01 +0000

    Setup of more gulp and css structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postCss = require('gulp-postcss'),
+autoPrefixer = require('autoprefixer'),
+cssVars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 
 gulp.task('default', function(){
@@ -11,8 +16,13 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-    console.log('Imagine this doing something very useful with that stylesheet');
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postCss([cssImport, cssVars, nested, autoPrefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
 });
+
+
+
 
 gulp.task('watch', function() {
 
